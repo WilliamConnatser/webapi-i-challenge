926b73c703381f39f3399fc29c0bd4097bbdda2c
WilliamConnatser <WilliamConnatser@users.noreply.github.com>
2019-04-22 17:32:31 -0500

    all endpoints functioning correctly

--- index.js
+++ index.js
@@ -37,36 +37,100 @@ server.post('/api/users', (req, res) => {
                 return db.findById(response.id);
             })
             .then(response => {
-                res.status(201).send({data: response});
+                res.status(201).send({
+                    data: response
+                });
             })
             .catch(error => {
                 res.status(500).send({
                     error: "There was an error while saving the user to the database"
-                })
+                });
             });
     }
 });
 
 server.get('/api/users/:id', (req, res) => {
     db.findById(req.params.id)
-    .then(response => {
-        if(response.length === 0) {
-            res.status(404).send({error: "The user with the specified ID does not exist."})
-        } else {
-            res.status(200).send({data: response})
-        }
-    })
-    .catch(error => {
-        res.status(500).send({error: "The user information could not be retrieved."})
-    });
+        .then(response => {
+            if (response.length === 0) {
+                res.status(404).send({
+                    error: "The user with the specified ID does not exist."
+                });
+            } else {
+                res.status(200).send({
+                    data: response
+                });
+            }
+        })
+        .catch(error => {
+            res.status(500).send({
+                error: "The user information could not be retrieved."
+            })
+        });
 });
 
 server.put('/api/users/:id', (req, res) => {
-    res.send('Sanity check');
+
+    let userEditing;
+
+    db.findById(req.params.id)
+        .then((response) => {
+            if (response === undefined) {
+                res.status(404).send({
+                    error: "The user with the specified ID does not exist."
+                });
+            } else if (req.body.bio === undefined || req.body.name === undefined) {
+                res.status(400).send({
+                    error: "Please provide name and bio for the user."
+                });
+            } else {
+                userEditing = response;
+                return db.update(req.params.id, {
+                    name: req.body.name,
+                    bio: req.body.bio
+                });
+            }
+        })
+        .then(response => {
+            return db.findById(userEditing.id);
+        })
+        .then(response => {
+            res.status(200).send({
+                data: response
+            });
+        })
+        .catch(error => {
+            res.status(500).send({
+                error: "The user information could not be modified."
+            })
+        });
 });
 
 server.delete('/api/users/:id', (req, res) => {
-    res.send('Sanity check');
+
+    let userDeleted;
+
+    db.findById(req.params.id)
+        .then((response) => {
+            if (response.length === 0) {
+                res.status(404).send({
+                    error: "The user with the specified ID does not exist."
+                });
+            } else {
+                userDeleted = response;
+                return db.remove(req.params.id);
+            }
+        })
+        .then(response => {
+            res.status(200).send({
+                data: userDeleted
+            });
+        })
+        .catch(error => {
+            res.status(500).send({
+                error: "The user could not be removed."
+            });
+        });
 });
 
 server.listen(5000, () => console.log('Server is running on localhost:5000'));
